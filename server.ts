46769d7cda0503ca76606986e29f3330a2873c15
hashrock <hashrock@users.noreply.github.com>
2024-03-26 11:20:57 +0900

    init

---
+++ server.ts
@@ -0,0 +1,10 @@
+const handler = (request: Request): Response => {
+  const body = `Your user-agent is:\n\n${
+    request.headers.get("user-agent") ?? "Unknown"
+  }`;
+
+  return new Response(body, { status: 200 });
+};
+
+console.log(`HTTP server running. Access it at: http://localhost:8080/`);
+Deno.serve({  }, handler);
